415734983bdaaa36324ae5fe57a331bec5bd5fcd
Karina <dddashkina.ka@gmail.com>
2025-03-24 18:34:32 +0300

    first part

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,3 +24,50 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+
+let placesList = document.querySelector('.places__list');
+const popupImageAttr = document.querySelector('.popup__image');
+const popupCaption = document.querySelector('.popup__caption');
+const imagePopup = document.querySelector('.popup_type_image');
+const closeButtonImage = imagePopup.querySelector('.popup__close');
+ 
+//Функция создания карточки
+function createCard(cardName, cardLink) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+ 
+  cardElement.querySelector('.card__title').textContent = cardName;
+  let likedButton = cardElement.querySelector('.card__like-button');
+  let deletedButton = cardElement.querySelector('.card__delete-button');
+ 
+  //Поставить или убрать лайк
+  likedButton.addEventListener("click", function() {
+    likedButton.classList.toggle('card__like-button_is-active');
+  });
+ 
+  //Кнопка удаления карточки
+  deletedButton.addEventListener("click", function() {
+    deletedButton.closest('.places__item').remove();
+  });
+ 
+  //Обработчик нажатия на карточку
+  cardImage.addEventListener("click", function() {
+    popupImageAttr.src = cardLink;
+    popupImageAttr.alt = cardName;
+    popupCaption.textContent = cardName;
+    openModal(imagePopup);
+  });
+ 
+  //Обработчик нажатия на кнопку закрытия карточки
+  closeButtonImage.addEventListener('click', function () {
+    closeModal(imagePopup);
+  });
+  return cardElement;
+}
+ 
+initialCards.forEach(function (item) {
+  placesList.append(createCard(item.name, item.link));
+});
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,66 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+let profilePopup = document.querySelector('.popup_type_edit');
+let cardPopup = document.querySelector('.popup_type_new-card');
+let imagePopup = document.querySelector('.popup_type_image');
+let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
+let closeButtonEdit = profilePopup.querySelector('.popup__close');
+let closeButtonCard = cardPopup.querySelector('.popup__close');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_description');
+ 
+let popup = document.querySelectorAll('.popup');
+ 
+popup.forEach((item) => {
+    item.classList.add('popup_is-animated');
+})
+ 
+function openModal(popup) {
+    popup.classList.add('popup_is-opened');
+}
+ 
+function closeModal(popup) {
+    popup.classList.remove('popup_is-opened');
+}
+ 
+editButton.addEventListener('click', function() {
+    nameInput.value = document.querySelector('.profile__title').textContent;
+    jobInput.value = document.querySelector('.profile__description').textContent;
+    openModal(profilePopup);
+});
+ 
+closeButtonEdit.addEventListener('click', function () {
+   closeModal(profilePopup);
+});
+ 
+const profileFormElement = profilePopup.querySelector('.popup__form');
+function handleProfileFormSubmit(evt) {
+     evt.preventDefault();
+     document.querySelector('.profile__title').textContent = nameInput.value;
+     document.querySelector('.profile__description').textContent = jobInput.value;
+     closeModal(profilePopup);
+}
+ 
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+ 
+addButton.addEventListener('click', function() {
+   let cardName = document.querySelector('.popup__input_type_card-name');
+   let cardURL = document.querySelector('.popup__input_type_url');
+   cardName.value = '';
+   cardURL.value = '';
+   openModal(cardPopup);
+});
+ 
+closeButtonCard.addEventListener('click', function () {
+   closeModal(cardPopup);
+});
+ 
+const cardFormElement = cardPopup.querySelector('.popup__form');
+function handlecardFormSubmit(evt) {
+    evt.preventDefault();
+    let cardName = document.querySelector('.popup__input_type_card-name');
+    let cardURL = document.querySelector('.popup__input_type_url');
+    placesList.prepend(createCard(cardName.value, cardURL.value));
+    closeModal(cardPopup);
+}
+ 
+cardFormElement.addEventListener('submit', handlecardFormSubmit);
